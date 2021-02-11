27ef079e9cfdf3598c1275fca0051984dccad268
shubhamsahu <sahu4816@gmail.com>
2021-02-11 14:24:21 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+         <h1 data-ns-test="project-name">List of best Movies</h1>
+         <p data-ns-test="project-description">In this site we give the reting of movies and provide some naration or review about movies</p>
+    </div>
   )
 }
 
